a11d793b4b8ff36a7f061d2bbb387ffbf9933383
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2025-01-07 14:27:14 +0100

    make templates and static directory inside django root directory.

--- srcs/django/confApp/views.py
+++ srcs/django/confApp/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-	return render(request, "pong/index.html")
+	return render(request, "tran_app/index.html")
---
+++ srcs/frontend/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Copy files after volume mount
+cp -r /tmp/templates/* /project/templates/
+cp -r /tmp/static/* /project/static/
