ea8ea75132af901e1430cded86a8ed800843fb42
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2025-01-07 17:37:31 +0100

    connect to database

--- srcs/postgresql/tools/setup_postgres.sh
+++ srcs/postgresql/tools/setup_postgres.sh
@@ -11,8 +11,7 @@ trap cleanup SIGINT SIGTERM
 chown -R postgres:postgres /var/lib/postgresql/data
 chmod 0700 /var/lib/postgresql/data
 chmod 0700 /var/lib/postgresql/data/*
-
-postgres -D /var/lib/postgresql/data &
+/usr/lib/postgresql/17/bin/postgres -D /var/lib/postgresql/data &
 POSTGRES_PID=$!
 
 until pg_isready -h localhost -p 5432; do
@@ -29,14 +28,10 @@ psql -U postgres <<EOF
     GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};
     \c ${POSTGRES_DB}
     GRANT ALL ON SCHEMA public TO ${POSTGRES_USER};
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ${POSTGRES_USER};
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO ${POSTGRES_USER};
 EOF
 
 echo "PostgreSQL setup completed"
-echo ${POSTGRES_USER}
-echo ${POSTGRES_PASSWORD}
-echo ${POSTGRES_DB}
 
-# psql -U postgres
-
-# postgres -D /var/lib/postgresql/data
 wait ${POSTGRES_PID}
