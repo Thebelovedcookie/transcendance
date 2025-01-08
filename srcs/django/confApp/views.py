f16e58a14331403515c958f5c48062c752eda0f8
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2025-01-08 18:58:16 +0100

    fix some error

--- srcs/django/confApp/views.py
+++ srcs/django/confApp/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, "tran_app/index.html")
+
+def home(request):
+	return render(request, "tran_app/home.html")
+
+def pong(request):
+	return render(request, "tran_app/pong/index.html")
+
+def pong_normal(request):
+	return render(request, "tran_app/pong/normal.html")
+
+def pong_solo(request):
+	return render(request, "tran_app/pong/solo.html")
