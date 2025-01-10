e667657c89a6f4f2119a6a722bda586c8ade911c
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2025-01-10 17:19:19 +0100

    Not using django to render(generate) html text. just show index.html file like react

--- srcs/django/confApp/urls.py
+++ srcs/django/confApp/urls.py
@@ -5,11 +5,6 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-	path("", views.index, name="index"),
-    path("home", views.home, name="home"),
-    path("pong", views.pong, name="pong"),
-    path("pong/normal", views.pong_normal, name="pong_normal"),
-    path("pong/solo", views.pong_solo, name="pong_solo"),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
