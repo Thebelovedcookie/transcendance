b5c4fb2e5e49cad3d548586daf6d02e7266f6ebf
mahfoud <marianne.picard.88@live.fr>
2025-01-10 13:35:42 +0100

    sound added

--- srcs/frontend/static/js/game_mode/normal/style.js
+++ srcs/frontend/static/js/game_mode/normal/style.js
@@ -3,37 +3,37 @@
 //raquette playerOne
 export function firstPaddle(context, element)
 {
-	context.fillStyle = element.color;
-	context.shadowColor = "rgba(255, 0, 255, 0.7)";
-	context.shadowOffsetX = 3;
-	context.shadowOffsetY = 1;
-	context.shadowBlur = 10;
+	// context.fillStyle = element.color;
+	// context.shadowColor = "rgba(255, 0, 255, 0.7)";
+	// context.shadowOffsetX = 3;
+	// context.shadowOffsetY = 1;
+	// context.shadowBlur = 10;
 
 	context.fillRect(element.x, element.y, element.width, element.height);
-	resetStyle(context);
+	// resetStyle(context);
 }
 
 //raquette playerTwo
 export function secondPaddle(context, element)
 {
-	context.fillStyle = element.color;
-	context.shadowColor = "rgba(0, 125, 255, 0.7)";
-	context.shadowOffsetX = -3;
-	context.shadowOffsetY = 1;
-	context.shadowBlur = 6;
+	// context.fillStyle = element.color;
+	// context.shadowColor = "rgba(0, 125, 255, 0.7)";
+	// context.shadowOffsetX = -3;
+	// context.shadowOffsetY = 1;
+	// context.shadowBlur = 6;
 
 	context.fillRect(element.x, element.y, element.width, element.height);
-	resetStyle(context);
+	// resetStyle(context);
 }
 
 //ball -> ronde + couleur + ombre
 export function ballStyle(context, element)
 {
-	context.fillStyle = element.color;
-	context.shadowColor = "rgba(0, 125, 255, 0.7)";
-	context.shadowOffsetX = 0;
-	context.shadowOffsetY = 0;
-	context.shadowBlur = 6;
+	// context.fillStyle = element.color;
+	// context.shadowColor = "rgba(0, 125, 255, 0.7)";
+	// context.shadowOffsetX = 0;
+	// context.shadowOffsetY = 0;
+	// context.shadowBlur = 6;
 
 	context.beginPath();
     context.arc(
@@ -44,7 +44,7 @@ export function ballStyle(context, element)
         Math.PI * 2 // Fin de l'angle (cercle complet)
     );
     context.fill();
-	resetStyle(context);
+	// resetStyle(context);
 }
 
 export function drawDashedLine(context, canvas) {
@@ -79,48 +79,52 @@ export function drawDashedLine(context, canvas) {
 
 //----------------------------TEXTE--------------------------------//
 
-let scaleFactor = 1;
-let scalingUp = true;
+// let scaleFactor = 1;
+// let scalingUp = true;
 
 //PlayerOne score Text
 export function displayScoreOne(context, scoreOne, canvas) {
-	if (scalingUp) {
-		scaleFactor += 0.001; // Augmente la taille
-		if (scaleFactor >= 1.1) scalingUp = false; // Arrête l'agrandissement
-	} else {
-		scaleFactor -= 0.01; // Diminue la taille
-    if (scaleFactor <= 1) scalingUp = true; // Arrête la réduction
-  }
+// 	if (scalingUp) {
+// 		scaleFactor += 0.001; // Augmente la taille
+// 		if (scaleFactor >= 1.1) scalingUp = false; // Arrête l'agrandissement
+// 	} else {
+// 		scaleFactor -= 0.01; // Diminue la taille
+//     if (scaleFactor <= 1) scalingUp = true; // Arrête la réduction
+//   }
 
-  context.font = `${30 * scaleFactor}px 'Arial', sans-serif`;
-  context.fillStyle = "#9f53ec";
-  context.textBaseline = "top";
-  context.shadowColor = "rgba(0, 125, 255, 255)";
-  context.shadowOffsetX = 1;
-  context.shadowOffsetY = 0;
-  context.shadowBlur = 3;
-  context.fillText(scoreOne, canvas.width / 2 - 60, 30);
+//   context.font = `90px 'Arial', sans-serif`;
+	context.font = "90px 'Press Start 2P'";
+  	// context.font = "48px Courier New";
+  	context.fillStyle = "#FFFFFF";
+  	context.textBaseline = "top";
+//   context.shadowColor = "rgba(0, 125, 255, 255)";
+//   context.shadowOffsetX = 1;
+//   context.shadowOffsetY = 0;
+//   context.shadowBlur = 3;
+  context.fillText(scoreOne, canvas.width / 2 - 120, 30);
   resetStyle(context);
 }
 
 //PlayerTwo score Text
 export function displayScoreTwo(context, scoreTwo, canvas){
-	if (scalingUp) {
-		scaleFactor += 0.001; // Augmente la taille
-	if (scaleFactor >= 1.1) scalingUp = false; // Arrête l'agrandissement
-	} else {
-		scaleFactor -= 0.01; // Diminue la taille
-		if (scaleFactor <= 1) scalingUp = true; // Arrête la réduction
-	}
+	// if (scalingUp) {
+	// 	scaleFactor += 0.001; // Augmente la taille
+	// if (scaleFactor >= 1.1) scalingUp = false; // Arrête l'agrandissement
+	// } else {
+	// 	scaleFactor -= 0.01; // Diminue la taille
+	// 	if (scaleFactor <= 1) scalingUp = true; // Arrête la réduction
+	// }
 	
-	context.font = `${30 * scaleFactor}px 'Arial', sans-serif`;
-	context.fillStyle = "#9f53ec";
+	// context.font = `90px 'Arial', sans-serif`;
+	// context.font = "48px Courier New";
+	context.font = "90px 'Press Start 2P'";
+	context.fillStyle = "#FFFFFF";
 	context.textBaseline = "top";
-	context.shadowColor = "rgba(0, 125, 255, 255)";
-	context.shadowOffsetX = -1;
-	context.shadowOffsetY = 0;
-	context.shadowBlur = 3;
-	context.fillText(scoreTwo, canvas.width / 2 + 45, 30);
+	// context.shadowColor = "rgba(0, 125, 255, 255)";
+	// context.shadowOffsetX = -1;
+	// context.shadowOffsetY = 0;
+	// context.shadowBlur = 3;
+	context.fillText(scoreTwo, canvas.width / 2 + 60, 30);
 	resetStyle(context);
 }
 	
