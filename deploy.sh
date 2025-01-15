dbd7743326e6d45820b34a48bf112995cfbb0c93
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2025-01-15 15:46:04 +0100

    delete deploy file

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Sync frontend files to data directory
-rsync -av --delete srcs/frontend/ data/frontend/ --dry-run
-echo "Are you sure? (y/n)"
-read -s -n 1 answer
-if [ "$answer" = "y" ]; then
-    rsync -av --delete srcs/frontend/ data/frontend/
-fi
