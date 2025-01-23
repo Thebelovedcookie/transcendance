0df36e9fe1829e3293dcf3f962f1a147e7e0f2b4
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2025-01-23 10:40:05 +0100

    create superuser, create custom user model using abstractUser

--- srcs/django/tools/setups_backend.sh
+++ srcs/django/tools/setups_backend.sh
@@ -7,6 +7,11 @@ trap cleanup SIGINT SIGTERM
 
 export PYTHONPATH=${PYTHONPATH}:/project
 
+export DJANGO_SUPERUSER_PASSWORD=12qwaszx
+python3.11 manage.py makemigrations
+python3.11 manage.py migrate
+python3.11 manage.py createsuperuser --noinput --username admin --email admin@admin.com
+
 # python3.11 /project/manage.py runserver 0.0.0.0:8000 &
 daphne backend_project.asgi:application -b 0.0.0.0 -p 8000 &
 DJANGO_PID=$!
