f82eab7d950944b9bb6028947a1f70b65bbba0ed
sonia O <sonia42@localhost.localdomain>
2025-02-16 12:24:47 +0100

    build ok avec langage; finir les fichier .po

--- srcs/django/backendProject/settings.py
+++ srcs/django/backendProject/settings.py
@@ -147,6 +147,7 @@ from django.utils.translation import gettext_lazy as _
 LANGUAGES = [
     ('en', _('English')),
     ('fr', _('French')),
+    ('es', _('Spanish')),
     ('ja', _('Japanese')),
 ]
 
