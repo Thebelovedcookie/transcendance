24c7c879f3115117323740da3a65461eff3a01d4
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2025-02-20 18:53:33 +0100

    setting for gmail

--- srcs/django/backend_project/settings.py
+++ srcs/django/backend_project/settings.py
@@ -192,9 +192,19 @@ CSRF_TRUSTED_ORIGINS = [
 	'https://127.0.0.1',
 ]
 
-# email settings
+# email settings for dev(console)
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 25
 DEFAULT_FROM_EMAIL = 'transcendence-pong@gmail.com'
 ACTIVATION_EXPIRED_MINUTES = 10
+
+# gmail settings for prod
+# DEFAULT_FROM_EMAIL = 'transcendence-pong@gmail.com'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# ACTIVATION_EXPIRED_MINUTES = 10
